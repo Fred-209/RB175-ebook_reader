d1fa4e63947983a511ab12fdd53bc0899561a814
Fred Durham <f.durham@protonmail.com>
2022-05-11 13:21:30 -0500

    Completed first lesson draft

--- book_viewer.rb
+++ book_viewer.rb
@@ -2,19 +2,62 @@ require 'tilt/erubis'
 require "sinatra"
 require "sinatra/reloader"
 
+before do 
+  @contents = File.readlines("data/toc.txt")
+  
+end
+
+not_found do 
+  redirect "/"
+end
+
+helpers do 
+  def in_paragraphs(str)
+    str.split("\n\n").map { |paragraph| "<p>#{paragraph}</p>"}.join
+  end
+
+  def text_found_in_chapter?(chapter, text)
+    chapter.match?(/#{text}/i)
+  end
+end
+
+get "/search" do 
+  @search_results = []
+  @search_terms = params[:query]
+  
+  if @search_terms
+    
+    @contents.size.times do |chapter_num|
+      chapter = File.read("data/chp#{chapter_num + 1}.txt")
+      @search_results << chapter_num if text_found_in_chapter?(chapter, @search_terms)
+    end
+  end
+
+  erb(:search)
+end
+
 get "/" do
   @title = "The Adventures of Sherlock Holmes"
-  @contents = File.readlines("data/toc.txt")
-
+  
   erb :home
 end
 
-get /\/chapters\/([0-9]+)/ do
-  @chapter_num = params['captures'].first
-  @title = "Chapter #{@chapter_num}"
-  @contents = File.readlines("data/toc.txt")
-  @chapter = File.read("data/chp#{@chapter_num}.txt")
+get "/chapters/:number" do
+  chapter_num = params[:number].to_i
+
+  redirect "/" unless (1..@contents.size).cover?(chapter_num)
+
+  chapter_name = @contents[chapter_num - 1]
+  @title = "Chapter #{chapter_num}: #{chapter_name}"
+
+  @chapter = File.read("data/chp#{chapter_num}.txt")
+
 
   erb :chapter
-  
 end
+
+get "/show/:name" do 
+  params[:name]
+end
+
+
